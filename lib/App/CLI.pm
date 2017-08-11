30fd2f7b21294fe27dcce660f0f9fcb638bfc001
José Joaquín Atria <jjatria@gmail.com>
2017-08-11 16:10:05 +0100

    Move app ref weakening to app method

--- lib/App/CLI.pm
+++ lib/App/CLI.pm
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use 5.006;
 use Class::Load qw( load_class );
-use Scalar::Util qw( weaken );
 
 our $VERSION = '0.45';
 
@@ -235,7 +234,6 @@ sub get_cmd {
     my @arg = %$data;
     $cmd = $pkg->new(@arg);
     $cmd->app($self);
-    weaken($cmd->{app});
     return $cmd;
 }
 
