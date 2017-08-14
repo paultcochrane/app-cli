aa684859de24b8fd40f05f80490b1ad17bb0f292
José Joaquín Atria <jjatria@gmail.com>
2017-08-14 15:29:46 +0100

    Guard against undef data in get_cmd

--- lib/App/CLI.pm
+++ lib/App/CLI.pm
@@ -278,7 +278,7 @@ sub get_cmd {
 
     die $self->error_cmd($cmd) unless $pkg->can('run');
 
-    my @arg = %$data;
+    my @arg = defined $data ? %$data : ();
     $cmd = $pkg->new(@arg);
     $cmd->app($self);
     return $cmd;
