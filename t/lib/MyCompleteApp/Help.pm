cb8dfbbe31a1cc3bd61642ab4b343a4a3a877358
Paul Cochrane <paul@liekut.de>
2017-11-05 12:56:02 +0100

    Add help text to MyCompleteApp::Help

--- t/lib/MyCompleteApp/Help.pm
+++ t/lib/MyCompleteApp/Help.pm
@@ -4,6 +4,13 @@ use base qw(App::CLI::Command::Help);
 use strict;
 use warnings;
 
+=head1 NAME
+
+MyCompleteApp::Help - help for the complete test app
+
+=cut
+
+
 sub run {
     my $self = shift;
     $self->SUPER::run(@_);
